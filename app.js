b870124a613cf5c49130a08a5efefe6254e70736
MarilyaVictoria <169046914+MarilyaVictoria@users.noreply.github.com>
2024-11-26 20:17:40 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
-function criacartao (categoria, pergunta, resposta) {
-    let container = document.getElementById ('conainer')
-    let cartao = document.createElement ('article')
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
     cartao.className = 'cartao'
 
-    cartao.innerHTML = ` 
+    cartao.innerHTML = `
     <div class="cartao__conteudo">
-    <h3>História</h3>
+    <h3>${categoria}</h3>
     <div class="cartao__conteudo__pergunta">
-        <p>Qual é o nome do presidente do Brasil que ficou conhecido como Jango?</p>
+        <p>${pergunta}</p>
     </div>
     <div class="cartao__conteudo__resposta">
-        <p>João Goulart</p>
+        <p>${resposta}</p>
+    </div>
     </div>
     `
+
+    let respostaEstaVisivel = false
+
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active', respostaEstaVisivel)
+    }
+
+cartao.addEventListener('click', viraCartao)
 
     container.appendChild(cartao)
 }
--- pergunta.js
+++ pergunta.js
@@ -1,5 +1,71 @@
-criacartao (
+criaCartao (
+    'Geografia',
+    'Quais são as fontes renováveis?',
+    'energia eólica, biomassa, hidroelétrica, termoelétrica.'
+)
+
+criaCartao (
     'História',
-    'O que é Java script',
-    'o '
+    'Qual é o nome do presidente do Brasil que ficou conhecido como Jango?',
+    'João Goulart'
+)
+
+criaCartao (
+    'Filosofia',
+    'De quem é a famosa frase “Penso, logo existo”?',
+    'Descartes'
+)
+
+criaCartao (
+    'Física',
+    'De onde é a invenção do chuveiro elétrico?',
+    'Brasil'
+)
+
+criaCartao (
+    'Biologia',
+    ' Normalmente, quantos litros de sangue uma pessoa tem? Em média, quantos são retirados numa doação de sangue?',
+    'Entre 4 a 6 litros. São retirados 450 mililitros'
+)
+
+criaCartao (
+    'Química',
+    'Qual o nome popular do cloreto de sódio?',
+    'Sal de cozinha.'
+)
+
+criaCartao (
+    'Geografia',
+    ' Em que país foi construído o Muro de Berlim?',
+    'Alemanha'
+)
+
+criaCartao (
+    'História',
+    'Em que ano foi usado um celular pela primeira vez no Brasil?',
+    '1990'
+)
+
+criaCartao (
+    'Português',
+    'Qual a personagem mais famosa de Maurício de Sousa?',
+    'Mônica'
+)
+
+criaCartao (
+    'Arte',
+    'Quem pintou Mona Lisa?',
+    'Leonardo da Vinci.'
+)
+
+criaCartao (
+    'Educação Física',
+    'O que representam os cinco anéis olímpicos?',
+    'As partes do mundo unidas pelo Olimpismo.'
+)
+
+criaCartao (
+    'geografia',
+    'Qual o planeta mais próximo do Sol?',
+    'Mercúrio.'
 )
